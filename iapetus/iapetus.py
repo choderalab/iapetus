c44a5464b81f72536f175eb4f5e2fffd9cda30be
John Chodera <john.chodera@choderalab.org>
2018-04-18 00:00:19 -0400

    Set up and minimize

--- iapetus/iapetus.py
+++ iapetus/iapetus.py
@@ -14,6 +14,9 @@ import mdtraj
 from simtk import openmm, unit
 from simtk.openmm import app
 
+from openmmtools.constants import kB
+from openmmtools import integrators
+
 def setup_calculation(gromacs_input_path=None, ligand_resseq=None):
     """Set up a calculationself.
 
@@ -57,18 +60,66 @@ def setup_calculation(gromacs_input_path=None, ligand_resseq=None):
         return mdtraj_refpdb.xyz[0,atom,:] * unit.nanometers
 
     ligand_atoms = mdtraj_topology.select('(residue {}) and (mass > 1.5)'.format(ligand_resseq))
-    plane_atoms = mdtraj_topology.select('((residue 342 and resname GLY) or (residue 97 and resname ASP) or (residue 184 and resname SER)) and (name CA)')
+    protein_atoms_to_restrain = mdtraj_topology.select('((residue 342 and resname GLY) or (residue 97 and resname ASP) or (residue 184 and resname SER)) and (name CA)')
     axis_center_atom = mdtraj_topology.select('residue 128 and resname ALA and name CA')[0]
     pore_top_atom = mdtraj_topology.select('residue 226 and resname SER and name CA')[0]
     pore_bottom_atom = mdtraj_topology.select('residue 88 and resname VAL and name CA')[0]
     print('ligand heavy atoms: {}'.format(ligand_atoms))
-    print('plane atoms (x,y,z): {}'.format(plane_atoms))
+    print('protein plane atoms to restraint (x,y,z): {}'.format(protein_atoms_to_restrain))
     print('axis center (x,z) : {} : {}'.format(axis_center_atom, get_coordinates(axis_center_atom)))
     print('pore top (y-max): {} : {}'.format(pore_top_atom, get_coordinates(pore_top_atom)))
     print('pore bottom (y-min): {} : {}'.format(pore_bottom_atom, get_coordinates(pore_bottom_atom)))
 
     if len(ligand_atoms) == 0:
         raise ValueError('Ligand residue name {} not found'.format(ligand_resseq))
+
+    # Run parameters
+    temperature = 310.0 * unit.kelvin
+    pressure = 1.0 * unit.atmospheres
+    collision_rate = 1.0 / unit.picoseconds
+    timestep = 4.0 * unit.femtoseconds
+
+    # Compute beta
+    kT = kB * temperature
+    beta = 1.0 / kT
+
+    # Add a barostat
+    print('Adding a barostat...')
+    barostat = openmm.MonteCarloBarostat(pressure, temperature)
+    system.addForce(barostat)
+
+    # TODO: Restrain protein atoms in space
+    print('Adding restraints to protein...')
+    sigma_protein = 2.0 * unit.angstroms # stddev of fluctuations of protein atoms
+    K_protein = kT / (sigma_protein**2) # spring constant
+    energy_expression = '(K/2)*((x-x0)^2 + (y-y0)^2 + (z-z0)^2);'
+    force = openmm.CustomExternalForce(energy_expression)
+    force.addGlobalParameter('K_protein', K_protein)
+    force.addPerParticleParameter('x0')
+    force.addPerParticleParameter('y0')
+    force.addPerParticleParameter('z0')
+    for particle_index in protein_atoms_to_restrain:
+        x0, y0, z0 = get_coordinates(particle_index) / unit.nanometers
+        force.addParticle(int(particle_index), [x0, y0, z0])
+    system.addForce(force)
+
+    # TODO: Create SamplerState for initial conditions
+
+    # TODO: Create ThermodynamicState for unperturbed system
+
+    # TODO: Create ThermodynamicStates for umbrella sampling
+
+    # DEBUG: Minimize
+    print('Minimizing...')
+    integrator = integrators.LangevinIntegrator(temperature, collision_rate, timestep)
+    context = openmm.Context(system, integrator)
+    context.setPositions(grofile.positions)
+    potential_energy = context.getState(getEnergy=True).getPotentialEnergy()
+    print('Initial energy: %10.3f kcal/mol'.format(potential_energy / unit.kilocalories_per_mole))
+    context = openmm.LocalEnergyMinimizer.minimize(context)
+    potential_energy = context.getState(getEnergy=True).getPotentialEnergy()
+    print('Final energy:   %10.3f kcal/mol'.format(potential_energy / unit.kilocalories_per_mole))
+    del context, integrator
 
 def main():
     """Set up and run a porin permeation PMF calculation.
